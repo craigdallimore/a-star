9aea5f00c5bfa0634dd28ce5ab029d2c13cc76d8
craigdallimore <decoy9697@gmail.com>
2020-12-09 22:16:50 +1300

    Draw four points

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 // @flow
 
-// - show an SVG in the terminal
-// - draw the SVG from a datastructure produced by node
-// - update the view over time
-// - allow for hotreloading of the node app
-// - show a nice grid
-// - add some random dots
-// - find a path from top left to bottom right using a*
-// - measure performance
+// - [ ] show an SVG in the terminal
+// - [ ] draw the SVG from a datastructure produced by node
+// - [ ] update the view over time
+// - [ ] allow for hotreloading of the node app
+// - [ ] show a nice grid
+// - [ ] add some random dots
+// - [ ] find a path from top left to bottom right using a*
+// - [ ] measure performance
 
-type Xyz = {
-  a: string,
-  b: boolean,
-};
+// ----------------------------------------------------------------------------
+// https://www.npmjs.com/package/sharp
 
-function test(xyz: Xyz): boolean {
-  return xyz.b;
+import * as ansiEscapes from "ansi-escapes";
+
+type Point = { x: number, y: number };
+
+type Points = Array<Point>;
+
+function show(points): void {
+  process.stdout.write(ansiEscapes.eraseScreen);
+  points.forEach(({ x, y }) => {
+    process.stdout.write(ansiEscapes.cursorTo(x, y));
+    process.stdout.write(".");
+  });
 }
+
+const points: Points = [
+  { x: 0, y: 0 },
+  { x: 10, y: 0 },
+  { x: 10, y: 10 },
+  { x: 0, y: 10 },
+];
+
+show(points);
