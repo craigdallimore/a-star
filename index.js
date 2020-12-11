8eb3253616f5597d9af8b28a9966e3decc172f2c
craigdallimore <decoy9697@gmail.com>
2020-12-11 22:28:14 +1300

    Crudely add rocks

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // @flow
 
-// - [ ] add some random dots
 // - [ ] find a path from top left to bottom right using a*
 // - [ ] measure performance
 
@@ -9,6 +8,12 @@
 
 import * as ansiEscapes from "ansi-escapes";
 import R from "ramda";
+import random from "random";
+import seedrandom from "seedrandom";
+
+random.use(seedrandom("dent"));
+
+const GRID = 10;
 
 type Point = { x: number, y: number };
 
@@ -21,26 +26,69 @@ type State = {
 
 // Data -----------------------------------------------------------------------
 
-const nums: Array<number> = R.range(0, 5);
+const nums: Array<number> = R.range(0, GRID);
 
 const points: Points = nums
   .map((y) => nums.map((x) => ({ x: x * 2, y })))
   .flat();
 
+const rockIndexes = new Array(points.length).fill(false);
+
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+rockIndexes[random.int(0, GRID * GRID)] = true;
+
+console.log(rockIndexes);
+
 const initialState: State = {
   points,
+  rocks: rockIndexes,
   index: 0,
 };
 
 // Application ----------------------------------------------------------------
 
+function getChar(cursor, point, pointIndex) {
+  const cursorAtPoint = cursor.x === point.x && cursor.y === point.y;
+  const rockAtPoint = rockIndexes[pointIndex];
+
+  if (cursorAtPoint) {
+    return "☺";
+  }
+  if (rockAtPoint) {
+    return "*";
+  }
+  return ".";
+}
+
 function show(state): void {
   const c = state.points[state.index];
   process.stdout.write(ansiEscapes.eraseScreen);
-  state.points.forEach(({ x, y }) => {
+  state.points.forEach((p, pointIndex) => {
     try {
-      const char = c.x === x && c.y === y ? "*" : ".";
-      process.stdout.write(ansiEscapes.cursorTo(x, y));
+      const char = getChar(c, p, pointIndex);
+      process.stdout.write(ansiEscapes.cursorTo(p.x, p.y));
       process.stdout.write(char);
     } catch (e) {
       console.error(e);
