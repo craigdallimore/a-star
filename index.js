93dddea3903c5d24642f616a7f418f1479ea0d71
craigdallimore <decoy9697@gmail.com>
2021-01-04 20:51:57 +1300

    Add `yarn run app`

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // @flow
 import type { State } from "./types";
 import { initialState } from "./lib/state";
+import { print } from "./lib/print";
 
 // Application ----------------------------------------------------------------
 
 function loop(state: State): void {
+  print(state.board);
   setTimeout(() => {
     const nextState = { ...state };
     loop(nextState);
